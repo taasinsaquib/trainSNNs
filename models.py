1bc78efe61730b4b1386b06de03c84c599a99ac7
Taasin <taasin.saquib@gmail.com>
2021-12-06 22:19:31 -0800

    add hybrid spiking NN

--- models.py
+++ models.py
@@ -73,7 +73,7 @@ class LCN(nn.Module):
 				return angle
 
 class LCNSpiking(nn.Module):
-		def __init__(self, in_dim, out_dim, K, factor, num_layer, alpha, beta, use_cuda=True, spikeGrad=None, inhibition=False):
+		def __init__(self, in_dim, out_dim, K, factor, num_layer, alpha, beta, use_cuda=True, spikeGrad=None, inhibition=False, directOutput=False):
 				super(LCNSpiking, self).__init__()
 
 				# Initialize parameters
@@ -196,10 +196,86 @@ class LCNSpiking(nn.Module):
 							# print("2", x.shape)
 							del weight, bias, knn
 
-					angle = self.fc_angle(x)
+					if not directOutput:
+						angle = self.fc_angle(x)
+					else:
+						angle = x
 					# print("out", angle.shape)
 
 				return mem4_rec, spk4_rec, angle
+
+class LCNSpikingHybrid(nn.Module):
+  def __init__(self, num_spiking, in_dim, out_dim, K, factor, num_layer, alpha, beta, use_cuda=True, spikeGrad=None, inhibition=False):
+      super(LCNSpikingHybrid, self).__init__()
+
+      # SNN PART
+      self.num_spiking = num_spiking
+      self.snn = LCNSpiking(in_dim, out_dim, K, factor, num_spiking, alpha, beta, use_cuda, spikeGrad, inhibition)
+      
+      # ANN PART
+      self.dtype = torch.FloatTensor
+      self.weight_param, self.bias_param = nn.ParameterList(), nn.ParameterList()
+      self.knn_list = []
+      self.num_layer = num_layer
+      self.use_cuda = use_cuda
+    
+      dim = in_dim / (factor ** nSpiking)
+
+      # Initialize weight, bias, spiking neurons, and KNN data
+      for i in range(num_spiking, num_layer):
+          dim = int(math.floor(dim / factor))
+
+          # Weight and bias
+          w = torch.Tensor(dim, K).zero_().type(self.dtype).normal_(0, (2.0 / K) ** 0.5)
+          b = torch.zeros(1, dim).type(self.dtype)
+          self.weight_param.append(torch.nn.Parameter(w, requires_grad=True))
+          self.bias_param.append(torch.nn.Parameter(b, requires_grad=True))
+
+          # KNN
+          h5f = h5py.File('./KNN/%d/knn_index_%d.h5' % (K, i), 'r')
+          k_nearest = torch.from_numpy(h5f['data'][:]).type(torch.long)
+          h5f.close()
+
+          self.knn_list.append(k_nearest)
+
+      self.fc_angle = nn.Linear(dim, out_dim)
+
+  def forward(self, input):
+
+    x = input
+    batch_size = input.shape[0]
+
+    # SNN PART
+    _, _, x = self.snn(x)
+
+    print(x.size())
+
+    # ANN PART
+    for i in range(0, self.num_layer-self.num_spiking):
+        # print(len(self.weight_param), len(self.bias_param), len(self.knn_list))
+        if self.use_cuda:
+            weight, bias, knn = self.weight_param[i], self.bias_param[i], self.knn_list[i].cuda()
+        else:
+            weight, bias, knn = self.weight_param[i], self.bias_param[i], self.knn_list[i]
+        # print("1", x.unsqueeze(1).shape)
+        x = x.unsqueeze(1).expand(-1, weight.shape[0], -1)
+        # print("1", x.shape, knn.shape)
+        knn = knn.unsqueeze(0).expand(batch_size, -1, -1)
+        # print("2", knn.shape)
+        # print(x.shape, knn.shape)
+        x = torch.gather(x, 2, knn)
+        # print("3", x.shape, weight.unsqueeze(0).expand(batch_size, -1, -1).shape)
+        # print(x.get_device())
+        # print(weight.get_device())
+        x = x * weight.unsqueeze(0).expand(batch_size, -1, -1)
+        x = F.relu(torch.sum(x, 2) + bias)
+        del weight, bias, knn
+
+    angle = self.fc_angle(x)
+    print(angle.size())
+    # classification = self.fc_class(x)
+    return angle
+
 
 # TODO
 """
@@ -215,8 +291,9 @@ class LCNSpiking(nn.Module):
 
 def main():
 
-	linet         = LCN(14400, 2, 15, 2, 5, True)
-	spiking_linet = LCNSpiking(14400, 2, 15, 2, 5, 0.9, 0.8, True)
+	linet          = LCN(14400, 2, 15, 2, 5, True)
+	spiking_linet  = LCNSpiking(14400, 2, 15, 2, 5, 0.9, 0.8, True)
+	spiking_hybrid = LCNSpiking4L(4, 14400, 2, 15, 2, 5, 0, 0.25, True)
 
 if __name__ == "__main__":
 	main()
